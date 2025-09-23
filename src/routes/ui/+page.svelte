c4a49645173799cfdc3a0e46dcbd5b7f3ee1a5c3
Baekhyeon <baeks215@gmail.com>
2025-09-23 17:03:19 +0100

    feat(choice): add simple showcase for choices

---
+++ src/routes/ui/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import ChoiceShowcase from './helpers/ChoiceShowcase.svelte';
+</script>
+
+<ChoiceShowcase />
---
+++ src/routes/ui/helpers/ChoiceShowcase.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import { Choice, ChoiceMultiple } from '$lib/components/index.js';
+	import { identity } from 'ramda';
+
+	const items: string[] = ['item1', 'item2', 'item3', 'item4', 'item5'];
+
+	let valueOne: string | undefined = $state();
+	let valueOneV: string | undefined = $state();
+	let valueMultiple: string[] = $state([]);
+	let valueMultipleV: string[] = $state([]);
+</script>
+
+<h1>Choice</h1>
+<div class="grid grid-cols-2 gap-4 p-4">
+	<div>
+		Single: {valueOne}
+		<Choice bind:value={valueOne} {items} getKey={identity} getLabel={identity} />
+	</div>
+
+	<div>
+		Multiple: {valueMultiple}
+		<ChoiceMultiple
+			bind:value={valueMultiple}
+			{items}
+			getKey={identity}
+			getLabel={identity}
+			compareItems={(a, b) => a.localeCompare(b)}
+		/>
+	</div>
+
+	<div>
+		Single Vertical: {valueOneV}
+		<Choice bind:value={valueOneV} {items} getKey={identity} getLabel={identity} vertical />
+	</div>
+
+	<div>
+		Multiple Vertical: {valueMultipleV}
+		<ChoiceMultiple
+			bind:value={valueMultipleV}
+			{items}
+			getKey={identity}
+			getLabel={identity}
+			compareItems={(a, b) => a.localeCompare(b)}
+			vertical
+		/>
+	</div>
+</div>

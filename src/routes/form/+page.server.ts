eb4c4cae6c593cf959b45e01d9c86a15dafc1590
Baekhan Sung <55633208+mountwo23@users.noreply.github.com>
2025-10-03 23:08:40 +0100

    patch: textfield nullable

--- src/routes/form/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import { arktype } from 'sveltekit-superforms/adapters';
-import { superValidate } from 'sveltekit-superforms';
-import { masterSchema } from './master-schema.js';
-
-export const load = async () => {
-	const form = await superValidate(arktype(masterSchema));
-
-	return { form };
-};

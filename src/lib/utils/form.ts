2dd6ddf5f042bf08a4254c07d2322065f040878d
Baekhan Sung <55633208+mountwo23@users.noreply.github.com>
2025-10-09 13:59:13 +0100

    patch: fix fail server validate form

--- src/lib/utils/form.ts
+++ src/lib/utils/form.ts
@@ -1,13 +1,7 @@
 import { toast } from 'svelte-sonner';
 import { dateTransport } from '$lib/utils/index.js';
 import { type } from 'arktype';
-import {
-	defaults,
-	fail,
-	superForm,
-	superValidate,
-	type SuperValidated
-} from 'sveltekit-superforms';
+import { defaults, superForm, superValidate, type SuperValidated } from 'sveltekit-superforms';
 import { arktype, arktypeClient } from 'sveltekit-superforms/adapters';
 
 export function prepareForm<S extends type.Any<Record<string, unknown>>>(
@@ -82,13 +76,9 @@ export function prepareEmptyForm<S extends type.Any<Record<string, unknown>>>(
 	return { form, data: form.form, delayed, errors };
 }
 
-export async function serverValidateForm<S extends type.Any<Record<string, unknown>>>(
+export async function getServerForm<S extends type.Any<Record<string, unknown>>>(
 	request: Request,
 	schema: S
 ) {
-	const form = await superValidate(request, arktype(schema), { transport: dateTransport });
-	if (!form.valid) {
-		throw fail(400, { form });
-	}
-	return form;
+	return await superValidate(request, arktype(schema), { transport: dateTransport });
 }

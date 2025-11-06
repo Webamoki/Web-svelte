7409bd651188279d4d049118b25fb37d6a7e0cb4
Baekhan Sung <55633208+mountwo23@users.noreply.github.com>
2025-11-06 18:44:01 +0000

    Update form.ts

--- src/lib/utils/form.ts
+++ src/lib/utils/form.ts
@@ -26,14 +26,11 @@ export function prepareForm<S extends type.Any<Record<string, unknown>>>(
 				options?.onSuccess?.(form);
 			}
 			const text = form.message?.text;
-			if (text === undefined) return;
-
-			if (form.message?.showToast) {
-				if (form.message!.success) {
-					toast.success(text);
-				} else {
-					toast.error(text);
-				}
+			if (text === undefined || form.message?.showToast === false) return;
+			if (form.message!.success) {
+				toast.success(text);
+			} else {
+				toast.error(text);
 			}
 		},
 		onError({ result }) {
@@ -69,14 +66,11 @@ export function prepareEmptyForm<S extends type.Any<Record<string, unknown>>>(
 			}
 
 			const text = form.message?.text;
-			if (text === undefined) return;
-
-			if (form.message?.showToast) {
-				if (form.message!.success) {
-					toast.success(text);
-				} else {
-					toast.error(text);
-				}
+			if (text === undefined || form.message?.showToast === false) return;
+			if (form.message!.success) {
+				toast.success(text);
+			} else {
+				toast.error(text);
 			}
 		},
 		onError({ result }) {

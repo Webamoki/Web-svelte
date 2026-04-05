79948bc2b5d04bb0c0f8c3361cd9ff92188bd3a6
Baekhyeon <baeks215@gmail.com>
2026-04-05 02:24:23 +0100

    refactor(command): to toast on result

--- src/lib/shared/utils/command.ts
+++ src/lib/shared/utils/command.ts
@@ -2,25 +2,43 @@
 
 import type { RemoteCommand } from '@sveltejs/kit';
 
-import { ResponseResult } from './remote.js';
+import { toast } from 'svelte-sonner';
 
-export class CommandAction<S, O> {
+import type { CommandResult, CommandSuccess, ResponseError } from './remote.js';
+
+export class CommandAction<S, O extends CommandSuccess> {
   get submitting(): boolean {
     return this.#submitCount > 0;
   }
 
-  #remote: RemoteCommand<S, Promise<ResponseResult<O>>>;
+  #remote: RemoteCommand<S, Promise<CommandResult<O>>>;
   #submitCount = $state(0);
+  #toastError: boolean;
+  #toastSuccess: boolean;
 
-  constructor(remote: RemoteCommand<S, Promise<ResponseResult<O>>>) {
+  constructor(
+    remote: RemoteCommand<S, Promise<CommandResult<O>>>,
+    config?: {
+      toastError?: boolean;
+      toastSuccess?: boolean;
+    }
+  ) {
     this.#remote = remote;
+    this.#toastError = config?.toastError ?? false;
+    this.#toastSuccess = config?.toastSuccess ?? false;
   }
 
-  async execute(input: Parameters<RemoteCommand<S, Promise<ResponseResult<O>>>>[0]): Promise<O> {
+  async execute(input: Parameters<RemoteCommand<S, Promise<CommandResult<O>>>>[0]): Promise<O> {
     this.#submitCount++;
     try {
       const result = await this.#remote(input);
-      return ResponseResult.unwrap(result);
+      if (result.ok) {
+        if (this.#toastSuccess) toast.success(result.value.message);
+        return result.value;
+      } else {
+        if (this.#toastError) toast.error(`${result.error.code} - ${result.error.message}`);
+        throw result.error;
+      }
     } finally {
       this.#submitCount--;
     }

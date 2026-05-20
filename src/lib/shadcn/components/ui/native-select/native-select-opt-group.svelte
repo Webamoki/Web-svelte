47c3ae150212a19eaf368666c65901dc1da6ee27
Baekhyeon Sung <70176664+Baeks215@users.noreply.github.com>
2026-05-20 02:38:15 +0100

    fix(select): change to native select (#179)
    * build: shadcn remove select, add native select
    
    * fix: use native select for better responsive ui
    
    * fixup! fix: use native select for better responsive ui

---
+++ src/lib/shadcn/components/ui/native-select/native-select-opt-group.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import type { WithElementRef } from '$lib/shadcn/utils.js';
+  import type { HTMLOptgroupAttributes } from 'svelte/elements';
+
+  let {
+    children,
+    ref = $bindable(null),
+    ...restProps
+  }: WithElementRef<HTMLOptgroupAttributes> = $props();
+</script>
+
+<optgroup bind:this={ref} data-slot="native-select-opt-group" {...restProps}>
+  {@render children?.()}
+</optgroup>
